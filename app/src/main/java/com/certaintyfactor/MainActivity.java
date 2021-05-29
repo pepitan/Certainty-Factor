db3ccb52c77cc41b7833a141941b30e990e78567
jayana_putra_24rpl@student.smktelkom-mlg.sch.id <drevxluv2@gmail.com>
2021-05-29 13:52:29 +0700

    PERTAMA

---
+++ app/src/main/java/com/certaintyfactor/MainActivity.java
@@ -0,0 +1,22 @@
+package com.certaintyfactor;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.ib_penyakit).setOnClickListener(v -> startActivity(
+                new Intent(MainActivity.this, Penyakit.class)));
+
+        findViewById(R.id.ib_gejala).setOnClickListener(v -> startActivity(
+                new Intent(MainActivity.this, Gejala.class)));
+    }
+}
---
+++ app/src/main/java/com/certaintyfactor/Penyakit.java
@@ -0,0 +1,14 @@
+package com.certaintyfactor;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Penyakit extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_penyakit);
+    }
+}
