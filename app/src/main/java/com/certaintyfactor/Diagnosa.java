c1f80af64cacee387f2d54b412fc40051edcfbb3
jayana_putra_24rpl@student.smktelkom-mlg.sch.id <drevxluv2@gmail.com>
2021-07-14 20:02:18 +0700

    FINAL

--- app/src/main/java/com/certaintyfactor/Diagnosa.java
+++ app/src/main/java/com/certaintyfactor/Diagnosa.java
@@ -3,7 +3,6 @@ package com.certaintyfactor;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
@@ -203,10 +202,152 @@ public class Diagnosa extends AppCompatActivity {
             String persentase = "Persentase kepercayaan : ";
 
             if (cb1.isChecked() && cb3.isChecked() && cb4.isChecked() && cb5.isChecked()
-            && cb6.isChecked() && cb7.isChecked() && cb8.isChecked() && cb10.isChecked() && cb15.isChecked()
-            && cb17.isChecked()){
+                    && cb6.isChecked() && cb7.isChecked() && cb8.isChecked()
+                    && cb10.isChecked() && cb15.isChecked()  && cb17.isChecked()){
 
+                double nilaig1 = 0.4;
+                double nilaig3 = 1;
+                double nilaig4 = 0.6;
+                double nilaig5 = 0.2;
+                double nilaig6 = 0.8;
+                double nilaig7 = 0.4;
+                double nilaig8 = 0.4;
+                double nilaig10 = 0.4;
+                double nilaig15 = 0.2;
+                double nilaig17 = 0.6;
+
+                double dblgejala1 = Double.parseDouble(txt1.getText().toString());
+                double dblgejala3 = Double.parseDouble(txt3.getText().toString());
+                double dblgejala4 = Double.parseDouble(txt4.getText().toString());
+                double dblgejala5 = Double.parseDouble(txt5.getText().toString());
+                double dblgejala6 = Double.parseDouble(txt6.getText().toString());
+                double dblgejala7 = Double.parseDouble(txt7.getText().toString());
+                double dblgejala8 = Double.parseDouble(txt8.getText().toString());
+                double dblgejala10 = Double.parseDouble(txt10.getText().toString());
+                double dblgejala15 = Double.parseDouble(txt15.getText().toString());
+                double dblgejala17 = Double.parseDouble(txt17.getText().toString());
+
+                double hasilg1 = nilaig1 * dblgejala1;
+                double hasilg3 = nilaig3 * dblgejala3;
+                double hasilg4 = nilaig4 * dblgejala4;
+                double hasilg5 = nilaig5 * dblgejala5;
+                double hasilg6 = nilaig6 * dblgejala6;
+                double hasilg7 = nilaig7 * dblgejala7;
+                double hasilg8 = nilaig8 * dblgejala8;
+                double hasilg10 = nilaig10 * dblgejala10;
+                double hasilg15 = nilaig15 * dblgejala15;
+                double hasilg17 = nilaig17 * dblgejala17;
+
+                double cf1_cf3 = hasilg1 + hasilg3 * (1 - hasilg1);
+                double cfold_cf4 = cf1_cf3 + hasilg4 * (1 - cf1_cf3);
+                double cfold_cf5 = cfold_cf4 + hasilg5 * (1 - cfold_cf4);
+                double cfold_cf6 = cfold_cf5 + hasilg6 * (1 - cfold_cf5);
+                double cfold_cf7 = cfold_cf6 + hasilg7 * (1 - cfold_cf6);
+                double cfold_cf8 = cfold_cf7 + hasilg8 * (1 - cfold_cf7);
+                double cfold_cf10 = cfold_cf8 + hasilg10 * (1 - cfold_cf8);
+                double cfold_cf15 = cfold_cf10 + hasilg15 * (1 - cfold_cf10);
+                double cfold_cf17 = cfold_cf15 + hasilg17 * (1 - cfold_cf15);
+
+                String hasilhitungA = String.valueOf((cfold_cf17 * 100));
+
+                penyakit += "\nPenyakit Periodontitis";
+                persentase += "\n"+hasilhitungA;
             }
+
+            if (cb1.isChecked() && cb3.isChecked() && cb4.isChecked() && cb6.isChecked()
+                    && cb7.isChecked() && cb8.isChecked() && cb11.isChecked() && cb12.isChecked()
+                    && cb13.isChecked() && cb17.isChecked()){
+
+                double nilaig1 = 0.4;
+                double nilaig3 = 1;
+                double nilaig4 = 0.6;
+                double nilaig6 = 0.8;
+                double nilaig7 = 0.4;
+                double nilaig8 = 0.4;
+                double nilaig11 = 0.2;
+                double nilaig12 = 0.4;
+                double nilaig13 = 1;
+                double nilaig17 = 0.6;
+
+                double dblgejala1 = Double.parseDouble(txt1.getText().toString());
+                double dblgejala3 = Double.parseDouble(txt3.getText().toString());
+                double dblgejala4 = Double.parseDouble(txt4.getText().toString());
+                double dblgejala6 = Double.parseDouble(txt6.getText().toString());
+                double dblgejala7 = Double.parseDouble(txt7.getText().toString());
+                double dblgejala8 = Double.parseDouble(txt8.getText().toString());
+                double dblgejala11 = Double.parseDouble(txt11.getText().toString());
+                double dblgejala12 = Double.parseDouble(txt12.getText().toString());
+                double dblgejala13 = Double.parseDouble(txt13.getText().toString());
+                double dblgejala17 = Double.parseDouble(txt17.getText().toString());
+
+                double hasilg1 = nilaig1 * dblgejala1;
+                double hasilg3 = nilaig3 * dblgejala3;
+                double hasilg4 = nilaig4 * dblgejala4;
+                double hasilg6 = nilaig6 * dblgejala6;
+                double hasilg7 = nilaig7 * dblgejala7;
+                double hasilg8 = nilaig8 * dblgejala8;
+                double hasilg11 = nilaig11 * dblgejala11;
+                double hasilg12 = nilaig12 * dblgejala12;
+                double hasilg13 = nilaig13 * dblgejala13;
+                double hasilg17 = nilaig17 * dblgejala17;
+
+                double cf1_cf3_ = hasilg1 + hasilg3 * (1 - hasilg1);
+                double cfold_cf4 = cf1_cf3_ + hasilg4 * (1 - cf1_cf3_);
+                double cfold_cf6 = cfold_cf4 + hasilg6 * (1 - cfold_cf4);
+                double cfold_cf7 = cfold_cf6 + hasilg7 * (1 - cfold_cf6);
+                double cfold_cf8 = cfold_cf7 + hasilg8 * (1 - cfold_cf7);
+                double cfold_cf11 = cfold_cf8 + hasilg11 * (1 - cfold_cf8);
+                double cfold_cf12 = cfold_cf11 + hasilg12 * (1 - cfold_cf11);
+                double cfold_cf13 = cfold_cf12 + hasilg13 * (1 - cfold_cf12);
+                double cfold_cf17 = cfold_cf13 + hasilg17 * (1 - cfold_cf13);
+
+                String hasilhitungB = String.valueOf((cfold_cf17 * 100));
+
+                penyakit += "\nPenyakit Gingivitis";
+                persentase += "\n"+hasilhitungB;
+            }
+
+            if (cb2.isChecked() && cb3.isChecked() && cb6.isChecked() && cb9.isChecked()
+                    && cb13.isChecked() && cb14.isChecked() && cb16.isChecked()){
+
+                double nilaig2 = 0.2;
+                double nilaig3 = 1;
+                double nilaig6 = 0.8;
+                double nilaig9 = 0.2;
+                double nilaig13 = 1;
+                double nilaig14 = 0.2;
+                double nilaig16 = 0.4;
+
+                double dblgejala2 = Double.parseDouble(txt2.getText().toString());
+                double dblgejala3 = Double.parseDouble(txt3.getText().toString());
+                double dblgejala6 = Double.parseDouble(txt6.getText().toString());
+                double dblgejala9 = Double.parseDouble(txt9.getText().toString());
+                double dblgejala13 = Double.parseDouble(txt13.getText().toString());
+                double dblgejala14 = Double.parseDouble(txt14.getText().toString());
+                double dblgejala16 = Double.parseDouble(txt16.getText().toString());
+
+                double hasilg2 = nilaig2 * dblgejala2;
+                double hasilg3 = nilaig3 * dblgejala3;
+                double hasilg6 = nilaig6 * dblgejala6;
+                double hasilg9 = nilaig9 * dblgejala9;
+                double hasilg13 = nilaig13 * dblgejala13;
+                double hasilg14 = nilaig14 * dblgejala14;
+                double hasilg16 = nilaig16 * dblgejala16;
+
+                double cf1_cf3_1 = hasilg2 + hasilg3 * (1 - hasilg2);
+                double cfold_cf6 = cf1_cf3_1 + hasilg6 * (1 - cf1_cf3_1);
+                double cfold_cf9 = cfold_cf6 + hasilg9 * (1 - cfold_cf6);
+                double cfold_cf13 = cfold_cf9 + hasilg13 * (1 - cfold_cf9);
+                double cfold_cf14 = cfold_cf13 + hasilg14 * (1 - cfold_cf13);
+                double cfold_cf16 = cfold_cf14 + hasilg16 * (1 - cfold_cf14);
+
+                String hasilhitungC = String.valueOf((cfold_cf16 * 100));
+
+                penyakit += "\nPenyakit Abses Gusi";
+                persentase += "\n"+hasilhitungC;
+            }
+
+            txtHasil.setText(""+penyakit+"\n"+persentase+" %\n\n");
         });
 
     }
